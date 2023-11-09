12f7bd3266f0f082cd24605fa847c4699af72971
yeseong31 <yeseong31@naver.com>
2023-11-09 22:31:28 +0900

    feat: `ChristmasException` 예외 정의

---
+++ src/main/java/christmas/exception/ChristmasException.java
@@ -0,0 +1,20 @@
+package christmas.exception;
+
+public class ChristmasException extends IllegalArgumentException{
+
+    private ChristmasException(ErrorMessage errorMessage, Exception exception) {
+        super(errorMessage.getMessage(), exception);
+    }
+
+    private ChristmasException(ErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
+    }
+
+    public static ChristmasException of(ErrorMessage errorMessage, Exception exception) {
+        return new ChristmasException(errorMessage, exception);
+    }
+
+    public static ChristmasException from(ErrorMessage errorMessage) {
+        return new ChristmasException(errorMessage);
+    }
+}
