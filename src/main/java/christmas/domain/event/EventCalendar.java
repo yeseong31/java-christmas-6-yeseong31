fcb2ba42553c06e86813949224df80e6ab8be2f9
yeseong31 <yeseong31@naver.com>
2023-11-13 22:35:02 +0900

    feat: 이벤트 달력 도메인 `EventCalendar` 기능 구현

---
+++ src/main/java/christmas/domain/event/EventCalendar.java
@@ -0,0 +1,119 @@
+package christmas.domain.event;
+
+import static christmas.domain.event.constants.EventType.CHRISTMAS_EVENT;
+import static christmas.domain.event.constants.EventType.HOLIDAY_EVENT;
+import static christmas.domain.event.constants.EventType.SPECIAL_EVENT;
+import static christmas.domain.event.constants.EventType.WEEKDAY_EVENT;
+import static christmas.domain.event.constants.EventType.values;
+import static christmas.service.Parser.convertToDayOfWeek;
+import static christmas.service.Parser.convertToLocalDate;
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.SATURDAY;
+import static java.util.Collections.unmodifiableList;
+
+import christmas.domain.event.constants.EventType;
+import christmas.domain.order.Order;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+public class EventCalendar {
+
+    private static final List<Integer> specialDays = List.of(3, 10, 17, 24, 25, 31);
+    private static final String eventMenuName = "샴페인";
+    private static final int eventMenuAmount = 1;
+
+    private final List<EventType> events;
+    private final Order eventMenu;
+
+    private EventCalendar(final List<EventType> events, final Order eventMenu) {
+        this.events = events;
+        this.eventMenu = eventMenu;
+    }
+
+    public static EventCalendar of(final int dayOfMonth) {
+        final List<EventType> events = receiveApplicableEvents(dayOfMonth);
+        final Order eventMenu = generateEventMenu();
+        return new EventCalendar(events, eventMenu);
+    }
+
+    public List<EventType> receiveTargetEvents() {
+        return unmodifiableList(events);
+    }
+
+    public String receiveEventMenuInfo() {
+        return eventMenu.toString();
+    }
+
+    public static int receiveEventMenuPrice() {
+        return generateEventMenu().receiveOrderPrice();
+    }
+
+    public static boolean isChristmasPeriod(int dayOfMonth) {
+        return isDayOfMonthWithinRange(CHRISTMAS_EVENT, dayOfMonth);
+    }
+
+    public static boolean isSpecialDay(final int dayOfMonth) {
+        return specialDays.contains(dayOfMonth);
+    }
+
+    public static boolean isWeekday(final int dayOfMonth) {
+        return !isHoliday(dayOfMonth);
+    }
+
+    public static boolean isHoliday(final int dayOfMonth) {
+        final LocalDate reserveDate = convertToLocalDate(dayOfMonth);
+        final DayOfWeek dayOfWeek = convertToDayOfWeek(reserveDate);
+
+        return isFriday(dayOfWeek) || isSaturday(dayOfWeek);
+    }
+
+    private static boolean isSaturday(DayOfWeek dayOfWeek) {
+        return dayOfWeek == SATURDAY;
+    }
+
+    private static boolean isFriday(DayOfWeek dayOfWeek) {
+        return dayOfWeek == FRIDAY;
+    }
+
+    private static Order generateEventMenu() {
+        return Order.from(eventMenuName, eventMenuAmount);
+    }
+
+    private static List<EventType> receiveApplicableEvents(final int dayOfMonth) {
+        return Arrays.stream(values())
+                .filter(eventType -> isDayOfMonthWithinRange(eventType, dayOfMonth))
+                .filter(eventType -> isApplicableEvent(eventType, dayOfMonth))
+                .toList();
+    }
+
+    private static boolean isApplicableEvent(final EventType eventType, final int dayOfMonth) {
+        if (eventType == CHRISTMAS_EVENT) {
+            return isChristmasPeriod(dayOfMonth);
+        }
+        if (eventType == WEEKDAY_EVENT) {
+            return isWeekday(dayOfMonth);
+        }
+        if (eventType == HOLIDAY_EVENT) {
+            return isHoliday(dayOfMonth);
+        }
+        if (eventType == SPECIAL_EVENT) {
+            return isSpecialDay(dayOfMonth);
+        }
+        return false;
+    }
+
+    private static boolean isDayOfMonthWithinRange(final EventType eventType, final int dayOfMonth) {
+        return isGreaterThanStartEventDay(eventType, dayOfMonth)
+                && isSmallerThanEndEventDay(eventType, dayOfMonth);
+    }
+
+    private static boolean isSmallerThanEndEventDay(final EventType eventType, final int dayOfMonth) {
+        return eventType.getEndDate() >= dayOfMonth;
+    }
+
+    private static boolean isGreaterThanStartEventDay(final EventType eventType, final int dayOfMonth) {
+        return eventType.getStartDate() <= dayOfMonth;
+    }
+}
