d30afc9bf9485309e819c2efe701db371ea942ec
yeseong31 <yeseong31@naver.com>
2023-11-14 02:51:35 +0900

    fix: 날짜 입력 시 예외 검증 누락 수정

--- src/main/java/christmas/view/input/InputValidator.java
+++ src/main/java/christmas/view/input/InputValidator.java
@@ -5,6 +5,7 @@ import static christmas.domain.event.constants.DateConstraint.DATE_UPPER_BOUND;
 import static christmas.exception.ErrorMessage.EMPTY_INTEGER_INPUT;
 import static christmas.exception.ErrorMessage.EMPTY_ORDERS_INPUT;
 import static christmas.exception.ErrorMessage.INVALID_DATE_RANGE;
+import static christmas.exception.ErrorMessage.INVALID_INTEGER_INPUT;
 import static christmas.exception.ErrorMessage.INVALID_ORDER_FORMAT;
 import static christmas.view.input.InputView.ORDER_MENU_SEPARATOR;
 import static java.lang.Integer.parseInt;
@@ -16,7 +17,16 @@ public class InputValidator {
 
     public static void validateDate(final String input) {
         validateContainWhiteSpace(input, EMPTY_INTEGER_INPUT);
+        validateInteger(input);
         validateDateRange(input);
+    }
+
+    private static void validateInteger(String input) {
+        try {
+            parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw ChristmasException.from(INVALID_INTEGER_INPUT);
+        }
     }
 
     public static void validateMenuAndAmountStrings(final String input) {
