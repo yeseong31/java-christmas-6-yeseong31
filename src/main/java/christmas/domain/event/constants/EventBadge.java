42dfc2e5d6a98a66fca41968e50ac20493c3f5e7
yeseong31 <yeseong31@naver.com>
2023-11-14 02:33:46 +0900

    feat: 이벤트 배지 `enum` 정의

---
+++ src/main/java/christmas/domain/event/constants/EventBadge.java
@@ -0,0 +1,35 @@
+package christmas.domain.event.constants;
+
+public enum EventBadge {
+
+    NONE_BADGE("없음", 0),
+    STAR_BADGE("별", 5000),
+    TREE_BADGE("트리", 10000),
+    SANTA_BADGE("산타", 20000),
+    ;
+
+    private final String name;
+    private final int basePrice;
+
+    EventBadge(String name, int basePrice) {
+        this.name = name;
+        this.basePrice = basePrice;
+    }
+
+    public static EventBadge receiveBadge(int totalBenefitPrice) {
+        if (totalBenefitPrice >= SANTA_BADGE.basePrice) {
+            return SANTA_BADGE;
+        }
+        if (totalBenefitPrice >= TREE_BADGE.basePrice) {
+            return TREE_BADGE;
+        }
+        if (totalBenefitPrice >= STAR_BADGE.basePrice) {
+            return STAR_BADGE;
+        }
+        return NONE_BADGE;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
