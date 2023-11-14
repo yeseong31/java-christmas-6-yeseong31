3c29ce10201e367246ceb4b9b55ce35e83b63faa
yeseong31 <yeseong31@naver.com>
2023-11-14 12:03:14 +0900

    feat: 사용자 주문 메뉴 및 개수 입력 기능 구현

--- src/main/java/christmas/domain/controller/EventController.java
+++ src/main/java/christmas/domain/controller/EventController.java
@@ -2,9 +2,11 @@ package christmas.domain.controller;
 
 import static christmas.view.output.OutputView.printErrorMessage;
 
+import christmas.domain.order.Orders;
 import christmas.exception.ChristmasException;
 import christmas.view.input.InputView;
 import christmas.view.output.OutputView;
+import java.util.List;
 
 public class EventController {
 
@@ -13,6 +15,23 @@ public class EventController {
 
     public static void start() {
         int date = receiveDate();
+        Orders orders = receiveOrders(date);
+    }
+
+    private static Orders receiveOrders(int date) {
+        OutputView.printAskOrderMenuAndAmount();
+
+        return repeatInputOrders(date);
+    }
+
+    private static Orders repeatInputOrders(int date) {
+        try {
+            List<String> menuAndAmounts = InputView.readMenuAndAmountStrings();
+            return Orders.from(menuAndAmounts, date);
+        } catch (ChristmasException exception) {
+            printErrorMessage(exception);
+            return repeatInputOrders(date);
+        }
     }
 
     private static int receiveDate() {
