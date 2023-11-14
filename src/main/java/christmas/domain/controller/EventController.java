4c8b6e464fe997e47ad0c74460d5408f595f2657
yeseong31 <yeseong31@naver.com>
2023-11-14 12:06:53 +0900

    feat: 이벤트 달력 생성 및 이벤트 진행 결과 출력 기능 구현

--- src/main/java/christmas/domain/controller/EventController.java
+++ src/main/java/christmas/domain/controller/EventController.java
@@ -2,6 +2,7 @@ package christmas.domain.controller;
 
 import static christmas.view.output.OutputView.printErrorMessage;
 
+import christmas.domain.event.EventCalendar;
 import christmas.domain.order.Orders;
 import christmas.exception.ChristmasException;
 import christmas.view.input.InputView;
@@ -16,6 +17,20 @@ public class EventController {
     public static void start() {
         int date = receiveDate();
         Orders orders = receiveOrders(date);
+        EventCalendar eventCalendar = EventCalendar.of(date);
+
+        showEventProcess(date, orders, eventCalendar);
+    }
+
+    private static void showEventProcess(int date, Orders orders, EventCalendar eventCalendar) {
+        OutputView.printPreviewEventBenefits(date);
+        OutputView.printOrderMenu(orders);
+        OutputView.printTotalOrderAmountBeforeDiscount(orders);
+        OutputView.printGiftMenu(eventCalendar, orders);
+        OutputView.printBenefitsDetails(eventCalendar, orders);
+        OutputView.printTotalBenefitPrice(eventCalendar, orders);
+        OutputView.printEstimatedPriceAfterDiscount(eventCalendar, orders);
+        OutputView.printEventBadge(eventCalendar, orders);
     }
 
     private static Orders receiveOrders(int date) {
