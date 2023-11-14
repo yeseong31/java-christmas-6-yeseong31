1353b16bf2c9a8ba4652fd7b24c22ad26afac333
yeseong31 <yeseong31@naver.com>
2023-11-14 12:35:58 +0900

    refactor: `OutputView` 메서드에 `static import` 적용

--- src/main/java/christmas/domain/controller/EventController.java
+++ src/main/java/christmas/domain/controller/EventController.java
@@ -1,12 +1,22 @@
 package christmas.domain.controller;
 
+import static christmas.view.output.OutputView.printAskOrderMenuAndAmount;
+import static christmas.view.output.OutputView.printAskReservationDate;
+import static christmas.view.output.OutputView.printBenefitsDetails;
 import static christmas.view.output.OutputView.printErrorMessage;
+import static christmas.view.output.OutputView.printEstimatedPriceAfterDiscount;
+import static christmas.view.output.OutputView.printEventBadge;
+import static christmas.view.output.OutputView.printGiftMenu;
+import static christmas.view.output.OutputView.printHelloPlanner;
+import static christmas.view.output.OutputView.printOrderMenu;
+import static christmas.view.output.OutputView.printPreviewEventBenefits;
+import static christmas.view.output.OutputView.printTotalBenefitPrice;
+import static christmas.view.output.OutputView.printTotalOrderAmountBeforeDiscount;
 
 import christmas.domain.event.EventCalendar;
 import christmas.domain.order.Orders;
 import christmas.exception.ChristmasException;
 import christmas.view.input.InputView;
-import christmas.view.output.OutputView;
 import java.util.List;
 
 public class EventController {
@@ -22,26 +32,26 @@ public class EventController {
         showEventProcess(date, orders, eventCalendar);
     }
 
-    private static void showEventProcess(int date, Orders orders, EventCalendar eventCalendar) {
-        OutputView.printPreviewEventBenefits(date);
-        OutputView.printOrderMenu(orders);
-        OutputView.printTotalOrderAmountBeforeDiscount(orders);
-        OutputView.printGiftMenu(eventCalendar, orders);
-        OutputView.printBenefitsDetails(eventCalendar, orders);
-        OutputView.printTotalBenefitPrice(eventCalendar, orders);
-        OutputView.printEstimatedPriceAfterDiscount(eventCalendar, orders);
-        OutputView.printEventBadge(eventCalendar, orders);
+    private static void showEventProcess(final int date, final Orders orders, final EventCalendar eventCalendar) {
+        printPreviewEventBenefits(date);
+        printOrderMenu(orders);
+        printTotalOrderAmountBeforeDiscount(orders);
+        printGiftMenu(eventCalendar, orders);
+        printBenefitsDetails(eventCalendar, orders);
+        printTotalBenefitPrice(eventCalendar, orders);
+        printEstimatedPriceAfterDiscount(eventCalendar, orders);
+        printEventBadge(eventCalendar, orders);
     }
 
-    private static Orders receiveOrders(int date) {
-        OutputView.printAskOrderMenuAndAmount();
+    private static Orders receiveOrders(final int date) {
+        printAskOrderMenuAndAmount();
 
         return repeatInputOrders(date);
     }
 
-    private static Orders repeatInputOrders(int date) {
+    private static Orders repeatInputOrders(final int date) {
         try {
-            List<String> menuAndAmounts = InputView.readMenuAndAmountStrings();
+            final List<String> menuAndAmounts = InputView.readMenuAndAmountStrings();
             return Orders.from(menuAndAmounts, date);
         } catch (ChristmasException exception) {
             printErrorMessage(exception);
@@ -50,8 +60,8 @@ public class EventController {
     }
 
     private static int receiveDate() {
-        OutputView.printHelloPlanner();
-        OutputView.printAskReservationDate();
+        printHelloPlanner();
+        printAskReservationDate();
 
         return repeatInputDate();
     }
